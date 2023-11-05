721a09d2985ad22608f771120bd2763dc647349a
Mehregan Abdi <mehreganabdix@gmail.com>
2023-11-05 20:20:41 +0330

    IService +DTO Added

---
+++ DTOs/EmailDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FileExplorer.DTOs
+{
+    public class EmailDTO
+    {
+        public string Subject { get; set; } = "No Subject";
+        public string Reciever { get; set; }
+        public string message { get; set; } = "No Message";
+    }
+}
---
+++ IService/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using FileExplorer.DTOs;
+
+namespace FileExplorer.IService
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(EmailDTO email);
+        Task<bool> SendRegistrationCodeByEmail(string UserId);
+    }
+}
