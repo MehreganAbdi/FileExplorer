8a67e52f526643efb918ade4d311c0e4d0d6bf33
Mehregan Abdi <mehreganabdix@gmail.com>
2023-11-08 09:29:26 +0330

    Services Added

---
+++ Services/FileEntityService.cs
@@ -0,0 +1,158 @@
+﻿using FileExplorer.DTOs;
+using FileExplorer.IRepository;
+using FileExplorer.IService;
+
+namespace FileExplorer.Services
+{
+    public class FileEntityService : IFileEntityService
+    {
+        private readonly IFileEntityRepository fileEntityRepository;
+        private readonly IDataTranformerService dataTranformerService;
+
+        public FileEntityService(IFileEntityRepository fileEntityRepository,
+                                 IDataTranformerService dataTranformerService)
+        {
+            this.fileEntityRepository = fileEntityRepository;
+            this.dataTranformerService = dataTranformerService;
+        }
+
+        public bool AddFileEntity(FileEntityDTO file)
+        {
+            return fileEntityRepository.AddFileEntity(dataTranformerService.ChangeFileEntityDTOToFileEntity(file));
+        }
+
+        public async Task<bool> AddFileEntityAsync(FileEntityDTO file)
+        {
+            return await fileEntityRepository.AddFileEntityAsync(await dataTranformerService.ChangeFileEntityDTOToFileEntityAsync(file));
+
+        }
+
+        public ICollection<FileEntityDTO> GetAll()
+        {
+            var fileEntities = fileEntityRepository.GetAll();
+
+            var fileEntitiesDTO = new List<FileEntityDTO>();
+
+            foreach (var file in fileEntities)
+            {
+                fileEntitiesDTO.Add(dataTranformerService.ChangeFileEntityToFileEntityDTO(file));
+            }
+
+            return fileEntitiesDTO;
+        }
+
+        public async Task<ICollection<FileEntityDTO>> GetAllAsync()
+        {
+            var fileEntities = await fileEntityRepository.GetAllAsync();
+
+            var fileEntitiesDTO = new List<FileEntityDTO>();
+
+            foreach (var file in fileEntities)
+            {
+                fileEntitiesDTO.Add(await dataTranformerService.ChangeFileEntityToFileEntityDTOAsync(file));
+            }
+
+            return fileEntitiesDTO;
+        }
+
+        public ICollection<FileEntityDTO> GetAllByType(string type)
+        {
+            var fileEntities = fileEntityRepository.GetAllByType(type);
+
+            var fileEntitiesDTO = new List<FileEntityDTO>();
+
+            foreach (var file in fileEntities)
+            {
+                fileEntitiesDTO.Add(dataTranformerService.ChangeFileEntityToFileEntityDTO(file));
+            }
+
+            return fileEntitiesDTO;
+        }
+
+        public async Task<ICollection<FileEntityDTO>> GetAllByTypeAsync(string type)
+        {
+            var fileEntities = await fileEntityRepository.GetAllByTypeAsync(type);
+
+            var fileEntitiesDTO = new List<FileEntityDTO>();
+
+            foreach (var file in fileEntities)
+            {
+                fileEntitiesDTO.Add(await dataTranformerService.ChangeFileEntityToFileEntityDTOAsync(file));
+            }
+
+            return fileEntitiesDTO;
+        }
+
+        public FileEntityDTO GetById(int Id)
+        {
+            return dataTranformerService.ChangeFileEntityToFileEntityDTO(fileEntityRepository.GetById(Id));
+        }
+
+        public FileEntityDTO GetByIdAsNoTracking(int Id)
+        {
+            return dataTranformerService.ChangeFileEntityToFileEntityDTO(fileEntityRepository.GetByIdAsNoTracking(Id));
+
+        }
+
+        public async Task<FileEntityDTO> GetByIdAsNoTrackingAsync(int Id)
+        {
+            return await dataTranformerService.ChangeFileEntityToFileEntityDTOAsync(await fileEntityRepository.GetByIdAsNoTrackingAsync(Id));
+        }
+
+
+        public async Task<FileEntityDTO> GetByIdAsync(int Id)
+        {
+            return await dataTranformerService.ChangeFileEntityToFileEntityDTOAsync(await fileEntityRepository.GetByIdAsync(Id));
+        }
+
+
+        public ICollection<FileEntityDTO> GetFilesByProjectId(int projectId)
+        {
+            var files = fileEntityRepository.GetFilesByProjectId(projectId);
+
+            var filesDTOs = new List<FileEntityDTO>();
+
+            foreach (var file in files)
+            {
+                filesDTOs.Add(dataTranformerService.ChangeFileEntityToFileEntityDTO(file));
+            }
+            return filesDTOs;
+        }
+
+        public async Task<ICollection<FileEntityDTO>> GetFilesByProjectIdAsync(int projectId)
+        {
+
+            var files = await fileEntityRepository.GetFilesByProjectIdAsync(projectId);
+
+            var filesDTOs = new List<FileEntityDTO>();
+
+            foreach (var file in files)
+            {
+                filesDTOs.Add(await dataTranformerService.ChangeFileEntityToFileEntityDTOAsync(file));
+            }
+            return filesDTOs;
+        }
+
+        public bool RemoveFileEntity(FileEntityDTO file)
+        {
+            return fileEntityRepository.RemoveFileEntity(dataTranformerService.ChangeFileEntityDTOToFileEntity(file));
+        }
+
+        public async Task<bool> RemoveFileEntityAsync(FileEntityDTO file)
+        {
+            return await fileEntityRepository.RemoveFileEntityAsync(await dataTranformerService.ChangeFileEntityDTOToFileEntityAsync(file));
+
+        }
+
+        public bool Update(FileEntityDTO file)
+        {
+            return fileEntityRepository.Update(dataTranformerService.ChangeFileEntityDTOToFileEntity(file));
+        }
+
+        public async Task<bool> UpdateAsync(FileEntityDTO file)
+        {
+            return await fileEntityRepository.UpdateAsync(await dataTranformerService.ChangeFileEntityDTOToFileEntityAsync(file));
+
+        }
+    }
+}
--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -7,70 +7,95 @@ namespace FileExplorer.Services
     public class ProjectService : IProjectService
     {
         private readonly IProjectRepository projectRepository;
+        private readonly IDataTranformerService dataTranformerService;
 
-        public ProjectService(IProjectRepository projectRepository)
+        public ProjectService(IProjectRepository projectRepository,
+                              IDataTranformerService dataTranformerService)
         {
             this.projectRepository = projectRepository;
+            this.dataTranformerService = dataTranformerService;
         }
 
         public bool AddProject(ProjectDTO project)
         {
-            throw new NotImplementedException();
+            return projectRepository.AddProject(dataTranformerService.ChangeProjectDTOToProject(project));
         }
 
-        public Task<bool> AddProjectAsync(ProjectDTO project)
+        public async Task<bool> AddProjectAsync(ProjectDTO project)
         {
-            throw new NotImplementedException();
+            return await projectRepository.AddProjectAsync(await dataTranformerService.ChangeProjectDTOToProjectAsync(project));
+
         }
 
         public ICollection<ProjectDTO> GetAll()
         {
-            throw new NotImplementedException();
+            var projects = projectRepository.GetAll();
+            var projectsDTOs = new List<ProjectDTO>();
+
+            foreach (var project in projects)
+            {
+                projectsDTOs.Add(dataTranformerService.ChangeProjectToProjectDTO(project));
+            }
+
+            return projectsDTOs;
         }
 
-        public Task<ICollection<ProjectDTO>> GetAllAsync()
+        public async Task<ICollection<ProjectDTO>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var projects = await projectRepository.GetAllAsync();
+            var projectsDTOs = new List<ProjectDTO>();
+
+            foreach (var project in projects)
+            {
+                projectsDTOs.Add(await dataTranformerService.ChangeProjectToProjectDTOAsync(project));
+            }
+
+            return projectsDTOs;
+
         }
 
         public ProjectDTO GetById(int Id)
         {
-            throw new NotImplementedException();
+            return dataTranformerService.ChangeProjectToProjectDTO(projectRepository.GetById(Id));
         }
 
         public ProjectDTO GetByIdAsNoTracking(int Id)
         {
-            throw new NotImplementedException();
+            return dataTranformerService.ChangeProjectToProjectDTO(projectRepository.GetById(Id));
+
         }
 
-        public Task<ProjectDTO> GetByIdAsNoTrackingAsync(int Id)
+        public async Task<ProjectDTO> GetByIdAsNoTrackingAsync(int Id)
         {
-            throw new NotImplementedException();
+            return await dataTranformerService.ChangeProjectToProjectDTOAsync(await projectRepository.GetByIdAsync(Id));
+
         }
 
-        public Task<ProjectDTO> GetByIdAsync(int Id)
+        public async Task<ProjectDTO> GetByIdAsync(int Id)
         {
-            throw new NotImplementedException();
+             return await dataTranformerService.ChangeProjectToProjectDTOAsync(await projectRepository.GetByIdAsync(Id));
         }
 
         public bool RemoveProject(ProjectDTO project)
         {
-            throw new NotImplementedException();
+            return projectRepository.RemoveProject(dataTranformerService.ChangeProjectDTOToProject(project));
         }
 
-        public Task<bool> RemoveProjectAsync(ProjectDTO project)
+        public async Task<bool> RemoveProjectAsync(ProjectDTO project)
         {
-            throw new NotImplementedException();
+            return await projectRepository.RemoveProjectAsync(await dataTranformerService.ChangeProjectDTOToProjectAsync(project));
+
         }
 
         public bool Update(ProjectDTO project)
         {
-            throw new NotImplementedException();
+            return projectRepository.Update(dataTranformerService.ChangeProjectDTOToProject(project));
         }
 
-        public Task<bool> UpdateAsync(ProjectDTO project)
+        public async Task<bool> UpdateAsync(ProjectDTO project)
         {
-            throw new NotImplementedException();
+            return await projectRepository.UpdateAsync(await dataTranformerService.ChangeProjectDTOToProjectAsync(project));
+
         }
     }
 }
