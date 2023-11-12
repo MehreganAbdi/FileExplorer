e65a7e41f3ed788640529ff394b59390095a722c
Mehregan Abdi <mehreganabdix@gmail.com>
2023-11-12 15:18:48 +0330

    Project js Fully Externalized

---
+++ wwwroot/js/Project.Edit/ProjectEdit.js
@@ -0,0 +1,28 @@
+function validation() {
+
+    var projectName = document.getElementById("projecteditformname").value;
+
+    if (projectName.length < 3 || projectName.length > 100) {
+        sweetAlert({
+            title: "project name must be between 3 and 100 characters",
+            text: "",
+            icon: "error",
+            timer: 5000,
+            showConfirmButton: false
+        });
+
+        return false;
+    } else {
+        sweetAlert({
+            title: "Done!",
+            text: "Project Updated",
+            icon: "success",
+            timer: 4000,
+            showConfirmButton: false
+        });
+        return true;
+    }
+}
+function CountCharsProject() {
+    document.getElementById("editporject_name_count").innerHTML = '100 / ' + document.getElementById("projecteditformname").value.length;
+}
